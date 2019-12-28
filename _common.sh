da2034490240a46d6c0a37e9f16dc73610853895
nicolas <nhomble@terpmail.umd.edu>
2019-12-27 21:51:24 -0500

    init

---
+++ _common.sh
@@ -0,0 +1,19 @@
+GROUPME_CONFIG=~/.groupme-shell
+GROUPME_API_BASE=https://api.groupme.com/v3
+
+function hasConfig(){
+    if [[ ! -e $GROUPME_CONFIG ]]; then
+        printf "Configuration=$GROUPME_CONFIG does not exist, did you ./login?\n"
+        exit 1
+    fi
+}
+
+# stark and wayne
+function _j() {
+    cmd="echo $1 | base64 --decode | jq -r $2"
+    eval $cmd
+}
+
+function _escape () {
+    echo "$@" | sed 's/"/\\"/g'
+}
