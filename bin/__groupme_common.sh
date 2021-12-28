ef7278f5e1054cddbbf60102867e657389510ad1
Restyled.io <commits@restyled.io>
2021-12-28 19:49:36 +0000

    Restyled by shfmt

--- bin/__groupme_common.sh
+++ bin/__groupme_common.sh
@@ -6,48 +6,49 @@ export GROUPME_API_BASE=https://api.groupme.com/v3
 LOG_DIR="$HOME/.local/groupme-shell"
 mkdir -p "$LOG_DIR"
 LOGS="$LOG_DIR/logs.txt"
-echo "Start" >> "$LOGS"
+echo "Start" >>"$LOGS"
 
 function logit() {
   local message="$1"
-  echo "$(date) :: $message" >> "$LOGS"
+  echo "$(date) :: $message" >>"$LOGS"
 }
 
-function hasConfig(){
-    if [[ ! -e $GROUPME_CONFIG ]]; then
-        printf "Configuration=$GROUPME_CONFIG does not exist.\nRun:\n\tgroupme-login\n"
-        exit 1
-    fi
+function hasConfig() {
+  if [[ ! -e $GROUPME_CONFIG ]]; then
+    printf "Configuration=$GROUPME_CONFIG does not exist.\nRun:\n\tgroupme-login\n"
+    exit 1
+  fi
 }
 
 # stark and wayne
 function _j() {
-    local input="$1"
-    local cmd="echo $input | base64 --decode | jq -r $2"
-    eval "$cmd"
+  local input="$1"
+  local cmd="echo $input | base64 --decode | jq -r $2"
+  eval "$cmd"
 }
 
-function _escape () {
-    echo "$@" | sed 's/"/\\"/g'
+function _escape() {
+  echo "$@" | sed 's/"/\\"/g'
 }
 
 # expects json response in b64
 function checkStatus() {
-    local b64_response="$1"
-    local status=""
+  local b64_response="$1"
+  local status=""
 
-    # base64 -i is meant to ignore garbage in coreutils
-    if [[ $(isMac) != "true" ]]; then
-      status=$(echo "$b64_response" \
-          | base64 -i --decode \
-          | jq -c '.meta.code'\
-      )
-    fi
+  # base64 -i is meant to ignore garbage in coreutils
+  if [[ $(isMac) != "true" ]]; then
+    status=$(
+      echo "$b64_response" |
+        base64 -i --decode |
+        jq -c '.meta.code'
+    )
+  fi
 
-    if [[ "401" == "$status" ]]; then
-        printf "Token is stale, we got a 401\n"
-        exit 1
-    fi
+  if [[ "401" == "$status" ]]; then
+    printf "Token is stale, we got a 401\n"
+    exit 1
+  fi
 }
 
 # platform safe formatter
